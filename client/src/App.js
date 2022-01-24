5b46be74df9b82067fc41eff1cfb325c59a51595
yunho <Yunho7058@github.com>
2022-01-24 14:20:12 +0900

    feature/login 구현

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,12 @@
-import './App.css';
-import Header from './Components/header.js';
+import React from 'react';
+import CreatePost from './Pages/CreatePost';
+import ReadPost from './Pages/ReadPost';
 
 function App() {
   return (
     <div className="App">
-      <div>
-        <Header />
-      </div>
-      <div>
-        <span></span>
-        <h4>대타 게시판</h4>
-      </div>
-      <div>
-        <div className="test_posts">게시판</div>
-        <div className="test_posts">게시판</div>
-        <div className="test_posts">게시판</div>
-        <div className="test_posts">게시판</div>
-        <div className="test_posts">게시판</div>
-        <div className="test_posts">게시판</div>
-        <div className="test_posts">게시판</div>
-        <div className="test_posts">게시판</div>
-        <div className="test_posts">게시판</div>
-        <div className="test_posts">게시판</div>
-        <div className="test_posts">게시판</div>
-        <div className="test_posts">게시판</div>
-        <div className="test_posts">게시판</div>
-        <div className="test_posts">게시판</div>
-      </div>
+      <CreatePost />
+      <ReadPost />
     </div>
   );
 }

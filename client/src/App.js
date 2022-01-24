1e6cc4dffbc3a2d28fd041fe9f1d7794a8780895
yunho <Yunho7058@github.com>
2022-01-24 16:21:18 +0900

    feature/login 구현

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import CreatePost from './Pages/CreatePost'
+import ReadPost from './Pages/ReadPost'
+
+
+function App() {
+  return (
+    <div className="App">
+      <CreatePost />
+      <ReadPost />
+
+    </div>
+  );
+}
+
+export default App;

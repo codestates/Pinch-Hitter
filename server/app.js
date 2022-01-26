73ffcc973bbf1b83486663cfdc00c3e3128a7bc7
hykeem <w.hawk.95@gmail.com>
2022-01-27 00:57:59 +0900

    server 안됨

--- server/app.js
+++ server/app.js
@@ -23,8 +23,8 @@ app.get('/',(req,res)=>{
 app.use(cookieParser());
 app.get('/auth', controllers.auth)
 app.get('/notice_board', controllers.notice_board_read)
-// app.get('/userinfo', controllers.userinfo);
-// app.post('/signup', controllers.signup);
+app.get('/userinfo', controllers.userinfo);
+app.post('/signup', controllers.signup);
 app.post('/login', controllers.login);
 app.post('/logout', controllers.logout);
 app.post('/notice_board', controllers.notice_board)

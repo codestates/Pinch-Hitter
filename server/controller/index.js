5c66a797515d5cf0a612359de1974f511aa35260
hykeem <w.hawk.95@gmail.com>
2022-01-28 00:02:55 +0900

    수정 까지

--- server/controller/index.js
+++ server/controller/index.js
@@ -4,5 +4,7 @@ module.exports = {
     signup: require('./user/signup'),
     logout: require('./user/logout'),
     notice_board: require('./notice_board/notice_board'),
-    notice_board_read: require('./notice_board/notice_board_read')
+    notice_board_read: require('./notice_board/notice_board_read'),
+    notice_board_edit: require('./notice_board/notice_board_edit'),
+    notice_board_delete: require('./notice_board/notice_board_delete')
 }; 

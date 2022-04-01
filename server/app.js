aaed2d8e03da0e084ca5095eb6db3368a9e76c25
Kangyong0527 <pgy0527@naver.com>
2022-04-01 15:49:15 +0900

    FIX app.js

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
-const fs = require("fs");
-const https = require("https");
+// const fs = require("fs");
+// const https = require("https");
+const port = process.env.PORT || 80;
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const express = require("express");
@@ -35,20 +36,24 @@ app.delete("/notice_board", controllers.notice_board_delete);
 // 만약 인증서 파일이 존재하지 않는경우, http 프로토콜을 사용하는 서버를 실행합니다.
 // 파일 존재여부를 확인하는 폴더는 서버 폴더의 package.json이 위치한 곳입니다.
 
-const HTTPS_PORT = process.env.HTTPS_PORT || 4000;
-let server;
-if (fs.existsSync("./key.pem") && fs.existsSync("./cert.pem")) {
-  const privateKey = fs.readFileSync(__dirname + "/key.pem", "utf8");
-  const certificate = fs.readFileSync(__dirname + "/cert.pem", "utf8");
-  const credentials = { key: privateKey, cert: certificate };
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
 
-  server = https.createServer(credentials, app);
-  server.listen(HTTPS_PORT, () =>
-    console.log("https server runnning", HTTPS_PORT)
-  );
-} else {
-  server = app.listen(HTTPS_PORT, () =>
-    console.log("http server runnning", HTTPS_PORT)
-  );
-}
-module.exports = server;
+// const HTTPS_PORT = process.env.HTTPS_PORT || 4000;
+// let server;
+// if (fs.existsSync("./key.pem") && fs.existsSync("./cert.pem")) {
+//   const privateKey = fs.readFileSync(__dirname + "/key.pem", "utf8");
+//   const certificate = fs.readFileSync(__dirname + "/cert.pem", "utf8");
+//   const credentials = { key: privateKey, cert: certificate };
+
+//   server = https.createServer(credentials, app);
+//   server.listen(HTTPS_PORT, () =>
+//     console.log("https server runnning", HTTPS_PORT)
+//   );
+// } else {
+//   server = app.listen(HTTPS_PORT, () =>
+//     console.log("http server runnning", HTTPS_PORT)
+//   );
+// }
+// module.exports = server;

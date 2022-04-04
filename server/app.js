6529866f118f4284bfcd05917d106148a84d545a
Kangyong0527 <pgy0527@naver.com>
2022-04-04 22:49:18 +0900

    FIX app.js

--- server/app.js
+++ server/app.js
@@ -7,8 +7,8 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(express.json());
-const controllers = require("./controller");
+// app.use(express.json());
+// const controllers = require("./controller");
 
 app.use(express.urlencoded({ extended: false }));
 app.use(
